8e6719da8841df35be64a37f9d584aee115c5d81
Michael Long <michaeltlong10@gmail.com>
2023-10-07 01:45:29 -0400

    built out random activity generator

--- WhyNotTry/ContentView.swift
+++ WhyNotTry/ContentView.swift
@@ -8,14 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    var activities = ["Archery", "Baseball", "Basketball", "Bowling", "Boxing", "Cricket", "Curling", "Fencing", "Golf", "Hiking", "Lacrosse", "Rugby", "Squash"]
+    var colors: [Color] = [.blue, .cyan, .gray, .green, .indigo, .mint, .orange, .pink, .purple, .red]
+
+    @State private var selected = "Rugby"
+    @State private var id = 1
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Spacer()
+            
+            Text("Why not try...")
+                .font(.largeTitle.bold())
+            
+            Spacer()
+        
+            VStack {
+                Circle()
+                    .fill(colors.randomElement() ?? .blue)
+                    .overlay(
+                        Image(systemName: "figure.\(selected.lowercased())")
+                            .font(.system(size: 144))
+                            .foregroundColor(.white)
+                    )
+                Text("\(selected)!")
+                    .font(.title)
+            }
+            .padding()
+            .transition(.slide)
+            .id(id)
+            
+            Spacer()
+            
+            Button("Try again") {
+                withAnimation(.easeInOut(duration: 1)) {
+                    selected = activities.randomElement() ?? "Rugby"
+                    id += 1
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            
+            Spacer()
         }
-        .padding()
     }
 }
 
