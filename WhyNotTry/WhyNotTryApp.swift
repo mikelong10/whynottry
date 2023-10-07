312ca28eb286ba7f067259135d6b506db69953ae
Michael Long <michaeltlong10@gmail.com>
2023-10-07 00:04:42 -0400

    Initial Commit

---
+++ WhyNotTry/WhyNotTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhyNotTryApp.swift
+//  WhyNotTry
+//
+//  Created by Michael Long on 10/7/23.
+//
+
+import SwiftUI
+
+@main
+struct WhyNotTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
